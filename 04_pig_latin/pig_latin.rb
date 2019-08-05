bff3d65f8eecb89b692ad197f56818f1f042bd72
Steven Parker <sparker81@gmail.com>
2019-08-05 00:45:15 -0400

    Completed pig_latin.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,13 @@
-#write your code here
+def translate(message)
+  words = message.split(' ')
+  words.map! { |word| 
+    while not word.start_with?('a', 'e', 'i', 'o', 'u')
+      word = word[1,word.size] + word[0]
+    end
+    if word[0] == 'u' and word[-1] == 'q'
+      word = word[1,word.size] + word[0]
+    end
+    word + 'ay'
+  }
+  return words.join(' ')
+end
