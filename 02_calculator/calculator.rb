f951606423492c0c359be138babc8891e6277bd7
Steven Parker <sparker81@gmail.com>
2019-08-04 20:31:58 -0400

    Completed basic calculator.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,13 @@
-#write your code here
+def add(a, b)
+  return a + b
+end
+
+def subtract(a, b)
+  return a - b
+end
+
+def sum(numbers)
+  total = 0
+  numbers.each { |x| total = total + x }
+  return total
+end
