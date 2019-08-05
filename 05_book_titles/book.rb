dfb48c58f8f868b5d5dd871bf220ecfbe0045322
Steven Parker <sparker81@gmail.com>
2019-08-05 12:26:14 -0400

    Completed book titles.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+  def initialize
+    @title = ''
+  end
+
+  def title=(new_title)
+    @title = titleize(new_title)
+  end
+
+  def title
+    @title
+  end
+
+  private
+
+    def titleize(phrase)
+      non_cap_words = %w(a an and the in of)
+      phrase.split(' ').map.with_index { |w,idx| idx == 0 ? w.capitalize : non_cap_words.include?(w) ? w : w.capitalize }.join(' ')
+    end
 end
