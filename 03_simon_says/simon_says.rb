fc356fcb6f4d94e3ccfb34d56bdeb8bceee7911e
Steven Parker <sparker81@gmail.com>
2019-08-04 23:52:09 -0400

    Completed simon_says.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,28 @@
-#write your code here
+def echo(say)
+  return say
+end
+
+def shout(yell)
+  return yell.upcase
+end
+
+def repeat(message, many = 2)
+  output = message
+  many = many - 1
+  many.times { output = output + ' ' + message }
+  return output
+end
+
+def start_of_word(word, value)
+  return word[0, value]
+end
+
+def first_word(sentence)
+  return sentence.split(' ')[0]
+end
+
+def titleize(sentence)
+  little_words = %w(and it the over)
+  words = sentence.split(' ').map.with_index { |w, idx| idx == 0 ? w.capitalize : little_words.include?(w) ? w : w.capitalize }
+  return words.join(' ')
+end
--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem 'rake', '~> 12.3'
 gem 'rspec', '~> 3.4'
+gem 'irb'
